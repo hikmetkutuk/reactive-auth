efc1f2f70153cc6f3b5883e4a77e1289f7792133
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-01-03 23:48:05 +0300

    initial commit

---
+++ src/main/java/com/reactiveauth/ReactiveAuthApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactiveauth/ReactiveAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
